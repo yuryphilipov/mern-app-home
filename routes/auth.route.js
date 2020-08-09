c5250c0d05c7e627142d67a897d0202108ee10ea
Yury Philipov <yury.philipov@gmail.com>
2020-08-09 16:07:43 +0800

    loginRouter

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/default.json");
 const User = require("../models/User");
 const router = Router();
 
@@ -45,6 +47,47 @@ router.post(
   }
 );
 
-router.post("/login", async (req, res) => {});
+router.post(
+  "/login",
+  [
+    check("email", "Некорректный email").normalizeEmail().isEmail(),
+    check("password", "Введите пароль").exists(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Некорректные данные при входе в систему",
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ message: "Пользовтель не найден!" });
+      }
+
+      const isMatch = bcrypt.compare(password, user.password);
+
+      if (!isMatch)
+        return res
+          .status(400)
+          .json({ message: "Неверный пароль, попробуйте еще раз!" });
+
+      const token = jwt.sign({ userId: user.id }, config.get("jwt"), {
+        expiresIn: "1h",
+      });
+      res.json({ token, userId: user.id });
+    } catch (e) {
+      res
+        .status(500)
+        .json({ message: "Что то пошло не так, попробуйте снова!" });
+    }
+  }
+);
 
 module.exports = router;
