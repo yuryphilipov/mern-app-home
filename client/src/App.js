dd51ae661565f627d7a2c971f19ae044c6679e88
Yury Philipov <yury.philipov@gmail.com>
2020-08-09 17:44:39 +0800

    added client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "materialize-css";
+import { useRoutes } from "./routes";
+
+function App() {
+  const routes = useRoutes(true);
+  return (
+    <Router>
+      <div className='container'>{routes}</div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/DetailPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DetailPage = () => {
+  return (
+    <div>
+      <h1>Detail Page</h1>
+    </div>
+  );
+};
+export default DetailPage;
